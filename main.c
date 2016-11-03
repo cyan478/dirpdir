96d8a41968949c0027f089bc70eaf1811851e903
Celine Yan <celine@Celines-iMac.home>
2016-11-03 02:20:34 -0400

    done?

--- main.c
+++ main.c
@@ -1,17 +1,3 @@
-/*
-You will write a program that will gather various information about a directory, 
-specifically:
-list all the files in the directory
-specify which files are directories (if any)
-show the total size of all the regular files the directory
-note that you do not have to recursively go through any subdirectories to find their size for this part 
-(unless you want to, but that is not a simple task)
-Have the program scan the current directory
-Possible enhancements:
-Recursively list the files in any subdirectories, update the total to include the total size of subdirectories
-Print out the size in a more readable format (like using KB, MB, GB for -byte prefixes)
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -32,15 +18,16 @@ int getSize(DIR *dir){
 	return ans;
 }
 
-void listDir(DIR *dir, int type){
+void listDir(char * path, int type){
+	DIR *dir = opendir(path);
 	struct dirent *file = readdir(dir);
 	while (file){
 		if (file->d_type == type)
 			printf("\t%s\n", file->d_name);
 		file = readdir(dir);
 	}
+	closedir(dir);
 }
-
 
 int main(){
 	char *path = ".";
@@ -51,10 +38,10 @@ int main(){
 	printf("Size: %d Bytes\n", getSize(dir));
 
 	printf("Directories: \n ");
-	listDir(dir, DT_DIR);
-	printf("Regular Files: \n ");
-	listDir(dir,DT_REG);
+	listDir(path,DT_DIR);
 
+	printf("Regular Files: \n ");
+	listDir(path,DT_REG);
 
 	return 0;
 
